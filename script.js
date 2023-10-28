2a7f7b5999bbe2e04d051cca7109cb3f24489d86
sadv11 <121139993+sadv11@users.noreply.github.com>
2023-10-28 02:04:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function ShowClub1(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Sporting CP<br>Appearences: 31<br>Goals: 5<br>Assists: 6"
+}
+function ShowClub2(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Man Utd (2003-2009)<br>Appearences: 292<br>Goals: 118<br>Assists: 69"
+}
+function ShowClub3(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Real Madrid<br>Appearences: 438<br>Goals: 450<br>Assists: 131"
+}
+function ShowClub4(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Juventus<br>Appearences: 134<br>Goals: 101<br>Assists: 64"
+}
+function ShowClub5(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Man Utd(2021-2022)<br>Appearences: 37<br>Goals: 24<br>Assists: 3"
+}
+function ShowClub6(){
+    document.getElementById("info").style.visibility="visible"
+    document.getElementById("info").innerHTML="Al Nassr<br>Appearences: 32<br>Goals: 28<br>Assists: 9"
+}
